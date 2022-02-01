9b42073bf53d8b98b9f158acc4f9a1de9fe30295
iasminsoares <iasmin.borges.soares@gmail.com>
2022-02-01 08:14:07 -0300

    Novo curso

---
+++ C#parte5_BibliotecasDLLs/ByteBank/ByteBank.Modelos/ClasseTesteModificadoresDeAcesso.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Modelos
+{
+    class ClasseTesteModificadoresDeAcesso
+    {
+        ModificadoresTeste teste = new ModificadoresTeste();
+        
+    }
+
+    class ClasseDerivada : ModificadoresTeste
+    {
+        public ClasseDerivada()
+        {
+            MetodoProtegido();
+        }
+    }
+    public class ModificadoresTeste
+    {
+        public void MetodoPublico()
+        {
+            //Visivel fora da classe "ModificadoresTeste"
+        }
+
+        private void MetodoPrivado()
+        {
+            //Visivel apenas na classe "ModificadoresTeste"
+        }
+
+        protected void MetodoProtegido()
+        {
+            //Visivel apenas na classe "ModificadoresTeste" e derivados
+        }
+
+        internal void MetodoInterno()
+        {
+            //Visivel apenas para a Biblioteca/Projeto ByteBank.Modelos
+        }
+    }
+}
---
+++ C#parte5_BibliotecasDLLs/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using Humanizer;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dataFimPagamento = new DateTime(2018, 8, 17);
+            // Data corrente no momento de execução do código
+            DateTime dataCorrente = DateTime.Now;
+            TimeSpan diferenca = dataFimPagamento - dataCorrente;
+
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+
+            Console.WriteLine(mensagem);
+            //DateTime dataFimPagamento = new DateTime(2022, 06, 1);
+            //DateTime dataAtual = DateTime.Now;
+
+            //TimeSpan diferencaDeDatas = dataFimPagamento - dataAtual;
+
+            //string mensagem = "Vencimento em: " + TimeSpanHumanizeExtensions.Humanize(diferencaDeDatas);
+
+            //Console.WriteLine(mensagem);
+            Console.ReadLine(); 
+            //ContaCorrente conta = new ContaCorrente(847, 489754);
+            
+            //Console.WriteLine(conta.Numero);
+
+
+            //FuncionarioAutenticavel carlos = null;
+            //carlos.Autenticar("ijdiejijdijde");
+
+        }
+
+    }
+}
